e79d907728eed041e415bf63afcd1aa1d599303b
zachyking <dominik.zachar@gmail.com>
2022-01-23 23:00:15 +0100

    remove template content, install dapp dependencies

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   reactStrictMode: true,
+  webpack: (config) => {
+    // config.output.webassemblyModuleFilename = 'static/wasm/[modulehash].wasm'
+
+    // Since Webpack 5 doesn't enable WebAssembly by default, we should do it manually
+    config.experiments = { asyncWebAssembly: true, layers: true }
+
+    return config
+  }
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,28 +7,22 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>TypeScript starter for Next.js</title>
-        <meta
-          name="description"
-          content="TypeScript starter for Next.js that includes all you need to build amazing apps"
-        />
+        <title>Faucet for Cardano native assets</title>
+        <meta name="description" content="Faucet for Cardano native assets" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to Cardano native assets faucet
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{` `}
-          <code className={styles.code}>pages/index.tsx</code>
+          Get started by connecting your wallet
         </p>
 
-        <p className={styles.description}>This is not an official starter!</p>
-
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
+          {/* <a href="https://nextjs.org/docs" className={styles.card}>
             <h2>Documentation &rarr;</h2>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
@@ -54,19 +48,20 @@ export default function Home() {
             <p>
               Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
-          </a>
+          </a> */}
         </div>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=typescript-nextjs-starter"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a href="https://cardano.org" target="_blank" rel="noopener noreferrer">
           Powered by{` `}
           <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            <Image
+              src="/cardano-logo.svg"
+              alt="Cardano Logo"
+              width={24}
+              height={24}
+            />
           </span>
         </a>
       </footer>
